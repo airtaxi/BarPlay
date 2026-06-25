b7252c3c206449690d68a9d55bb3ec66f019a952
Howon Lee <kck4156@kagamine-rin.com>
2026-06-25 15:54:29 +0900

    Exit immediately on WM_ENDSESSION to prevent SMTC from holding process during MSIX update

--- BarPlay/MainWindow.xaml.cs
+++ BarPlay/MainWindow.xaml.cs
@@ -1,20 +1,45 @@
 ﻿using Deskband11Lib.WinUI;
 using Microsoft.UI.Xaml;
+using System.Runtime.InteropServices;
+using WinUIEx;
 
 namespace BarPlay;
 
 public sealed partial class MainWindow : Window
 {
+    private const uint WindowEndSessionMessage = 0x0016;
+
     public TaskbarContentHost TaskbarContentHost { get; }
+
+    private readonly WindowSubclassProcedure _windowSubclassProcedure;
+
+    private delegate nint WindowSubclassProcedure(nint windowHandle, uint message, nint wParam, nint lParam, nuint subclassIdentifier, nuint referenceData);
+
+    [LibraryImport("comctl32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static partial bool SetWindowSubclass(nint windowHandle, WindowSubclassProcedure procedure, nuint subclassIdentifier, nuint referenceData);
+
+    [LibraryImport("comctl32.dll")]
+    private static partial nint DefSubclassProc(nint windowHandle, uint message, nint wParam, nint lParam);
 
     public MainWindow()
     {
         InitializeComponent();
 
         TaskbarContentHost ??= new TaskbarContentHost(this, (FrameworkElement)Content, new() { PreferredWidth = 380, PreferredHeight = 48 });
+
+        _windowSubclassProcedure = OnWindowSubclassProcedure;
+        _ = SetWindowSubclass(this.GetWindowHandle(), _windowSubclassProcedure, 1, 0);
     }
 
     public async Task PrepareTaskbarContentAsync() => await TaskbarContentHost.AttachWhenLayoutReadyAsync();
 
     private void OnWindowClosed(object sender, WindowEventArgs e) => TaskbarContentHost?.Dispose();
+
+    private nint OnWindowSubclassProcedure(nint windowHandle, uint message, nint wParam, nint lParam, nuint subclassIdentifier, nuint referenceData)
+    {
+        if (message == WindowEndSessionMessage && wParam != 0) Environment.Exit(0);
+
+        return DefSubclassProc(windowHandle, message, wParam, lParam);
+    }
 }
